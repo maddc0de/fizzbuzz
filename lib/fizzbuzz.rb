ffab4defcf826f777293ce1deed1d8cde89bb393
maddc0de <maddc0de@outlook.com>
2023-03-07 15:57:30 +0000

    final refactored code and rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,19 @@
+# The program can be passed a number.
+# When passed a number that is a multiple of 3, the program returns the message 'Fizz'.
+# When passed a number that is a multiple of 5, the program returns the message 'Buzz'.
+# When passed a number that is a multiple of both 3 and 5, the program ignores the previous 2 rules and returns the message 'FizzBuzz'.
+
 def fizzbuzz(number)
-  'fizz'
+  fizz = number % 3 == 0
+  buzz = number % 5 == 0
+  if fizz && buzz
+    'FizzBuzz'
+  elsif fizz
+    'Fizz'
+  elsif buzz
+    'Buzz'
+  else
+    number
+  end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,23 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end   
-end
+  it "return `Fizz` for number divisible by 3" do
+    expect(fizzbuzz(3)).to eq "Fizz"
+  end
+  
+  it 'returns "buzz" for number divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+  
+  it 'returns "fizzbuzz" for number divisible by 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
 
+  it 'returns `fizzbuzz` for number divisible by 3 and 5' do
+    expect(fizzbuzz(90)).to eq "FizzBuzz"
+  end
+  
+  it 'returs 1 when number given is 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
+end
